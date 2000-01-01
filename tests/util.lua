1e404d9fd10800a456a92f6fcafa4b45afdc99c1
Alexander Nasonov <alnsn@yandex.ru>
2000-01-01 01:14:46 +0000

    Simple module for testing.

---
+++ tests/util.lua
@@ -0,0 +1,25 @@
+local _M = {}
+local all_tests, failed_tests = 0, 0
+
+function _M.exit()
+	if failed_tests > 0 then
+		io.stderr:write(string.format("FAILED: %d out of %d tests failed\n", failed_tests, all_tests))
+		os.exit(0)
+	else
+		io.stdout:write(string.format("PASSED: %d tests passed\n", all_tests))
+		os.exit(1)
+	end
+end
+
+function _M.assert(cond, ...)
+	all_tests = all_tests + 1
+	if not cond then
+		failed_tests = failed_tests + 1
+		local fmt = ...
+		local msg = fmt and string.format(...) or "assertion failed"
+		local _, err = pcall(error, msg, 3)
+		io.stderr:write(err, "\n")
+	end
+end
+
+return _M
