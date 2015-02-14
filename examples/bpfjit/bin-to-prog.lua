03bcd493413d47a08f14e5aa4862435f918c1350
Alexander Nasonov <alnsn@yandex.ru>
2015-02-14 14:31:00 +0000

    Add a repair flag.

--- examples/bpfjit/bin-to-prog.lua
+++ examples/bpfjit/bin-to-prog.lua
@@ -2,6 +2,9 @@
 -- Read filter programs from binary files and dump those programs
 -- to human readable format similar to an output of tcpdump -dd.
 
+-- Setting it to true might help to repair some malformed instructions.
+local repair = false
+
 local bpf = require "bpf"
 local args = {...}
 
@@ -17,7 +20,22 @@ for _,filename in ipairs(args) do
 		local code, jt, jf, k, newpos =
 		    string.unpack("<!1 I2 I1 I1 I4", data, pos)
 		pos = newpos
+
 		local valid, insn = pcall(bpf.stmt, code, k, jt, jf)
+
+		if repair and not valid then
+			local t = { code=code, jt=jt, jf=jf, k=k }
+			local converter = load("local bpf=...;return " ..
+			    bpf.M.tostring(t, "L"))
+			local valid, insn =  pcall(converter, bpf)
+			if valid then
+				code = insn.code
+				jt = insn.jt
+				jf = insn.jf
+				k = insn.k
+			end
+		end
+
 		local insnstr = valid and tostring(insn) or
 		    bpf.M.tostring({ code=code, jt=jt, jf=jf, k=k }, "L")
 		print(string.format("{0x%x, %d, %d, 0x%x}\t--\t%s",
